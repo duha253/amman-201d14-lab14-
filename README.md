15b445da62e9b07ac29aebab43310fe376764a23
duha253 <duhayousef26@gmail.com>
2021-02-25 01:21:24 +0200

    traying to solve lab14

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # amman-201d14-lab14-
+
+firstly in the cataloge 
+
+- we set up an  empty cart
+- we call this method to put all of the busmall options ,,we tried alot but i think these one areen solved it .
+we thougt we have to call the name from the array after we creat an element option .
+
+
+the second one the recuerd in it ,,, that we  need to add the selected item to the cart
+ object, save the whole thing back to local storage and update the screen
+ its took some time because selly error that we have put to **let** to the same function .
+ when we fount out the problem was solved .
+
+ the therd one need to selected item and quantity to the cart ,,its was hard and we searchh about it ,,,but when we solved it we think its an essy one .
+ 
+
--- js/ja.js
+++ js/ja.js
@@ -1,3 +1,5 @@
+/* global Product, Cart */
+
 'use strict';
 
 // Set up an empty cart for use on this page.
@@ -9,33 +11,27 @@ function populateForm() {
 
   //TODO: Add an <option> tag inside the form's select for each product
   const selectElement = document.getElementById('items');
- let option ;
+  let option ;
   for (let i in Product.allProducts) {
     option=document.createElement('option')
-    selectElement.appendChild(option);
-    option.textContent=Product.allProducts[i].name;
-    //console.log(Product.allProducts[i].name)
+    selectElement.appendChild(option)
+    option.textContent=Product.allProducts.name;
+
+   
   }
- 
 
 }
-
+let item;
+let quantity;
 // When someone submits the form, we need to add the selected item to the cart
 // object, save the whole thing back to local storage and update the screen
 // so that it shows the # of items in the cart and a quick preview of the cart itself.
-let item;
-let quantity; 
 function handleSubmit(event) {
-  event.preventDefault();
+ item =event.target.items.value;
+  quantity = event.target.quantity.value;
+
+
   // TODO: Prevent the page from reloading
-  // let item = event.terget.items.value;
- 
-  item = event.target.items.value;
- quantity = event.target.quantity.value;
-  // console.log(item);
-  // console.log(quantity);
-  // let item1 = parseInt( event.terget.quantity.value);
-  // console.log(item1);
 
   // Do all the things ...
   addSelectedItemToCart();
@@ -51,28 +47,21 @@ function addSelectedItemToCart() {
   // TODO: get the quantity
   // TODO: using those, add one item to the Cart
   cart.addItem(item,quantity);
-  console.log(cart);
+
+
 }
-let count=0;
+let count =0;
 // TODO: Update the cart count in the header nav with the number of items in the Cart
 function updateCounter() {
-  let counting =document.getElementById('itemCount')
-  count++;
-  counting.textContent=count;
+let counting = document .getElementById('itemCount')
+count++;
+counting.textContent = count;
 }
 
 // TODO: As you add items into the cart, show them (item & quantity) in the cart preview div
-const cartContent = document.getElementById('cartContents');
 function updateCartPreview() {
   // TODO: Get the item and quantity from the form
   // TODO: Add a new element to the cartContents div with that information
- 
-  let ulEl=document.createElement('ul');
-  cartContent.appendChild(ulEl);
-  let liEl=document.createElement('li');
-  ulEl.appendChild(liEl);
-  liEl.textContent=`${item} and quantity ${quantity} `
-  
 }
 
 // Set up the "submit" event listener on the form.
